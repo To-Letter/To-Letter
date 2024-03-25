cbfeff0194e5a2a0ba530cd6d7d6c29b19aa0fee
ovo <dbwjd20111@naver.com>
2024-03-25 16:09:55 +0900

    Fit : init

---
+++ To-Letter/src/main/java/com/toletter/ToLetterApplication.java
@@ -0,0 +1,13 @@
+package com.toletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToLetterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToLetterApplication.class, args);
+    }
+
+}
