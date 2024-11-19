69b55c9dccd1a552bbf6edaea7c58e98615d61f2
ovo <dbwjd20111@naver.com>
2024-11-19 10:13:52 +0900

    #7 feature : letter 편지 삭제 수정 보내는 편지, 받는 편지 모두 삭제되도록 수정함

--- To-Letter/src/main/java/com/toletter/Repository/SentBoxRepository.java
+++ To-Letter/src/main/java/com/toletter/Repository/SentBoxRepository.java
@@ -2,6 +2,10 @@ package com.toletter.Repository;
 
 import com.toletter.Entity.SentBox;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +16,8 @@ public interface SentBoxRepository extends JpaRepository<SentBox, Long> {
 
     List<SentBox> findAllByUserEmail(String user_email);
 
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM SentBox s WHERE s.letter.id in :letterIds")
+    void deleteAllByLetterIds(@Param("letterIds") List<Long> letterIds);
 }
