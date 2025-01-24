7e66cf2fcd241d82b042799755c9eeffb4443d03
DESKTOP-M3HIP2O\dbwjd <dbwjd20111@naver.com>
2025-01-24 11:35:15 +0900

    #31 fix : cors 오류 해결

--- To-Letter/src/main/java/com/toletter/Config/WebConfig.java
+++ To-Letter/src/main/java/com/toletter/Config/WebConfig.java
@@ -14,10 +14,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${front.api}")
     String frontApi;
 
+    @Value("${front.api2}")
+    String frontApi2;
+
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontApi)
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(frontApi, frontApi2, "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .exposedHeaders("Authorization", "Set-Cookie") // 헤더에 있는 값(토큰) 클라이언트 측에서 가져갈 수 있게 함.
                 .allowedHeaders("*")
